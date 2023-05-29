b7e1c6e545782c46532357f387c7120a857f4aa8
KFergusonUK <99019352+KFergusonUK@users.noreply.github.com>
2023-05-29 19:31:27 +0000

    Corrected Plotly map.  Some issues remain.
    Plotly now generates a map.
    Some issues in LG_Processed data, not clear if this is from conversion from LG file, or reading  from LG_Processed.csv

--- plotlyplot.py
+++ plotlyplot.py
@@ -2,56 +2,38 @@ def plotly_plot():
   
   import pandas as pd
   import plotly.graph_objects as go
-  import plotly.offline as offline
   
-  # Read the data from CSV file
-  df = pd.read_csv("LG_Processed.csv")
+  # Read the CSV file into a DataFrame
+  df = pd.read_csv('LG_Processed.csv')
   
-  # Extract coordinates and create LineString objects
-  lines = []
-  for index, row in df.iterrows():
-      coords = row['geometry'].replace('LINESTRING (', '').replace(')', '').split(', ')
-      line = []
-      for c in coords:
-          if ' ' in c:
-              line.append([float(coord) for coord in c.split()])
-          else:
-              line[-1].extend([float(coord) for coord in c.split()])
-      lines.append(line)
+  # Create a list to store the LineString coordinates
+  coordinates = []
   
-  # Create Plotly figure
-  fig = go.Figure()
-  for line in lines:
-      fig.add_trace(go.Scattermapbox(
-          lat=[point[1] for point in line],
-          lon=[point[0] for point in line],
-          mode='lines',
-          line=dict(width=3),
-          hovertemplate='USRN: %{text}',
-          text=[f"USRN: {row[0]}" for row in df.itertuples()],
-          hoverinfo='text',
-      ))
+  # Extract the LineString coordinates from the 'geometry' column
+  for geometry in df['geometry']:
+      coords = geometry.replace('LINESTRING (', '').replace(')', '').split(', ')
+      coords = [coord.split(' ') for coord in coords]
+      coords = [[float(coord[0]), float(coord[1])] for coord in coords]
+      coordinates.append(coords)
   
-  # Find the minimum and maximum coordinates for setting the map bounds
-  all_coords = [coord for line in lines for coord in line]
-  min_lon = min(coord[0] for coord in all_coords)
-  max_lon = max(coord[0] for coord in all_coords)
-  min_lat = min(coord[1] for coord in all_coords)
-  max_lat = max(coord[1] for coord in all_coords)
+  # Create a list to store the USRNs
+  usrns = df.iloc[:, 0]
   
-  # Set layout properties and zoom in on the relevant area
-  fig.update_layout(
-      mapbox=dict(
-          center=dict(lat=(min_lat + max_lat) / 2, lon=(min_lon + max_lon) / 2),
-          zoom=10
-      ),
-      showlegend=False
-  )
+  # Create a trace for each LineString
+  traces = []
+  for i in range(len(coordinates)):
+      x, y = zip(*coordinates[i])
+      trace = go.Scatter(x=x, y=y, mode='lines', name=f'USRN: {usrns[i]}')
+      traces.append(trace)
   
-  # Save the figure as a standalone HTML file
-  offline.plot(fig, filename='map.html', auto_open=False)
+  # Create the figure and set the layout
+  fig = go.Figure(data=traces, layout=go.Layout(title='Map Plot', showlegend=True))
   
-  # Open the HTML file in a separate browser tab
-  import webbrowser
-  webbrowser.open('map.html')
+  # Save the plot as an HTML file
+  output_path = 'map.html'
+  fig.write_html(output_path)
+  
+  # Print the output path for manual opening
+  print(f"Plot saved as '{output_path}'")
+  
   
